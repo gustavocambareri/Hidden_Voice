5fae5debc414fd580f25003d28883bda4bc52917
gustavocambareri <gustavo.cambareri@gmail.com>
2023-01-10 10:04:57 +0100

    peddiporcodio

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  circle(20, 20, 20);
+  textSize(200);
+  text("HIDDEN VOICE", width / 2, height / 2);
+}
